b268e7eda2054020bf1428b350908e0318d3a367
Nur-Alam-Limon <nuralam201425@gmail.com>
2025-06-21 18:47:25 +0600

    Created project, setup tailwind and header, added fonts

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className="bg-gradient-to-r from-pink-500 to-sky-400 text-sm text-white text-center py-2 font-medium">
+    🚀 FRESH BEGINNINGS SALE: Extra 25% OFF, Limited Spots - start your journey today!
+  </header>
+)
+
+export default Header;
